cadcdd251da9cf0cf9dc7b2875eb7cca2c7348d1
Aleksandr Krylov <a.krulov@gmail.com>
2011-11-12 22:39:26 +0400

    Frist commit tools (empty)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tools::Application.config.secret_token = 'd710e2721b356f1f5ca8965842fb356a22bb4af41af20c03af828c9264f0fe84d5cd29ff22705aed9da5af4acf64fddd888415d5d8d9de679d28d29740f6a80f'
